72103de01ca2317b0b56687bb5e1d3c58442d596
ulduz55 <165187822+ulduz55@users.noreply.github.com>
2024-04-20 12:53:24 +0400

    Add files via upload

---
+++ sənədlər 2024/faylyeni.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE mydatabase;
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+INSERT INTO users (id, name, age) VALUES (1, 'Alice', 25);
+INSERT INTO users (id, name, age) VALUES (2, 'Bob', 30);
+
---
+++ sənədlər 2024/js.js
@@ -0,0 +1,12 @@
+let a=15;
+let b=20;
+let c=13;
+if (a>b) {
+    console.log("a maksimumdur")
+}
+else if (b>c) {
+    console.log("b boyukdur")
+}
+else {
+    console.log("c boyukdur")
+} 
