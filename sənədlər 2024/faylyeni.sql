6e3703633efdb68d1c2ef19bea373d6cdea889ab
ulduz55 <165187822+ulduz55@users.noreply.github.com>
2024-06-15 12:12:42 +0400

    Delete sənədlər 2024 directory

--- sənədlər 2024/faylyeni.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE mydatabase;
-CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT
-);
-INSERT INTO users (id, name, age) VALUES (1, 'Alice', 25);
-INSERT INTO users (id, name, age) VALUES (2, 'Bob', 30);
-
